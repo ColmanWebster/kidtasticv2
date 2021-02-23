23d515a359f39fa98ea9d3c0ff160c0b33ad55eb
Gargantua <martinzb949@gmail.com>
2021-02-22 17:39:15 -0800

    updated and merged with the main branch

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ import View from "./pages/View";
 import Game from "./pages/Game";
 
 function App() {
-<<<<<<< HEAD
 	const [activeLink, setActiveLink] = useState("");
 	return (
 		<>
@@ -28,6 +27,7 @@ function App() {
 								<Route exact path="/" component={Home} />
 								<Route exact path="/home" component={Home} />
 								<Route exact path="/watch" component={Watch} />
+								<Route exact path="/watch/:id" component={View} />
 								<Route exact path="/login" component={Login} />
 								<Route exact path="/signup" component={Signup} />
 								<Route exact path="/dashboard" component={Dashboard} />
@@ -37,39 +37,10 @@ function App() {
 						</StoreProvider>
 					</div>
 				</Wrapper>
-				<Footer />
+				{/* <Footer /> */}
 			</Router>
 		</>
 	);
-=======
-  const [activeLink, setActiveLink] = useState("");
-  return (
-    <>
-      {" "}
-      <Router>
-        <Wrapper>
-          <div>
-            <StoreProvider>
-              <NewNav selected={activeLink} setActiveLink={setActiveLink} />
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/watch" component={Watch} />
-                <Route exact path="/watch/:id" component={View} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/game" component={Game} />
-                <Route component={NoMatch} />
-              </Switch>
-            </StoreProvider>
-          </div>
-        </Wrapper>
-        {/* <Footer /> */}
-      </Router>
-    </>
-  );
->>>>>>> f968f54533b8a4eb54e95b4534f5366e46607c64
 }
 
 export default App;
